ccfff0a21aded3af9d745d8ad5b18f8f43966263
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-09-10 20:16:43 -0400

    Got rid of = when matching upstream

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -25,8 +25,8 @@ set __fish_git_prompt_char_stagedstate '*'
 set __fish_git_prompt_char_untrackedfiles '…'
 set __fish_git_prompt_char_stashstate '↩'
 set __fish_git_prompt_char_upstream_prefix ''
-set __fish_git_prompt_char_upstream_ahead '↑'
-set __fish_git_prompt_char_upstream_behind '↓'
+set __fish_git_prompt_char_upstream_ahead '+'
+set __fish_git_prompt_char_upstream_behind '-'
 
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
