1f5c9ade404cb8e8b9a510d7f1ba6accec8bab5d
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-09-10 20:16:13 -0400

    Got rid of = when matching upstream

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -10,18 +10,23 @@ set __fish_git_prompt_showcolorhints 'yes'
 set __fish_git_prompt_showdirtystate 'yes'
 set __fish_git_prompt_showstashstate 'yes'
 set __fish_git_prompt_showuntrackedfiles 'yes'
-set __fish_git_prompt_showupstream 'yes'
 set __fish_git_prompt_color_branch normal
 set __fish_git_prompt_color_upstream_ahead green
 set __fish_git_prompt_color_upstream_behind red
 
+# Only shows upstream when not equal
+set __fish_git_prompt_showupstream 'informative'
+
 # Status Chars
-set __fish_git_prompt_char_dirtystate '⚡'
-set __fish_git_prompt_char_stagedstate '→'
-set __fish_git_prompt_char_untrackedfiles '*'
+set __fish_git_prompt_char_cleanstate '✔'
+set __fish_git_prompt_char_dirtystate '✖'
+set __fish_git_prompt_char_stateseparator ' '
+set __fish_git_prompt_char_stagedstate '*'
+set __fish_git_prompt_char_untrackedfiles '…'
 set __fish_git_prompt_char_stashstate '↩'
-set __fish_git_prompt_char_upstream_ahead '+'
-set __fish_git_prompt_char_upstream_behind '-'
+set __fish_git_prompt_char_upstream_prefix ''
+set __fish_git_prompt_char_upstream_ahead '↑'
+set __fish_git_prompt_char_upstream_behind '↓'
 
 function fish_prompt --description 'Write out the prompt'
 	set -l last_status $status
