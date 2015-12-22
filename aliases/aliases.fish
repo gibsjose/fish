53fd0a8ff091d5a14555c1eac2e2ea872665d835
Joe Gibson <gibsjose@mail.gvsu.edu>
2015-12-21 20:17:19 -0500

    Using blowfish algo and specifying key file

--- aliases/aliases.fish
+++ aliases/aliases.fish
@@ -1,4 +1,4 @@
 # Microamps Blog SSH
 function microamps
-    ssh -o ServerAliveInterval=20 microamps@microamps.ydns.eu $argv
+    ssh -o ServerAliveInterval=20 -c blowfish -i ~/.ssh/microamps microamps@microamps.ydns.eu $argv
 end
