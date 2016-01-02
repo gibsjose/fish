39029b5c969520fb7343eddba6760c4d407bb399
Joe Gibson <gibsjose@mail.gvsu.edu>
2016-01-02 12:15:11 -0500

    Fixing git repo subdirs

--- config.fish
+++ config.fish
@@ -1,23 +1,25 @@
 # PATH
 set -g -x PATH /usr/local/bin $PATH
+
 ### User Binaries
 set -g -x PATH ~/bin $PATH
-### General Coding Tools (clang, avr, etc.)
-set -g -x PATH ~/bin/clang-analyzer ~/bin/avr $PATH
-### Todo Python script
-set -g -x PATH ~/bin/todo $PATH
-### Pipsi
-set -g -x PATH ~/.local/bin $PATH
 ### Utilities
-set -g -x PATH ~/bin/utilities $PATH
+set -g -x PATH ~/bin/bash-utilities $PATH
 ### Git Merge/Diff Tools
 set -g -x PATH ~/bin/git-tools $PATH
+### Rocket
+set -g -x PATH ~/bin/rocket/bin $PATH
+### Todo Python script
+set -g -x PATH ~/bin/todo $PATH
+
+### General Coding Tools (clang, avr, etc.)
+set -g -x PATH ~/bin/clang-analyzer ~/bin/avr $PATH
+### Pipsi
+set -g -x PATH ~/.local/bin $PATH
 ### ROOT
 set -g -x PATH /usr/local/root/bin $PATH
 ### Add RVM to PATH for scripting
 set -g -x PATH $HOME/.rvm/bin $PATH
-### Rocket
-set -g -x PATH ~/bin/rocket/bin $PATH
 ### Go
 set -g -x GOPATH $HOME/go
 set -g -x PATH $GOPATH $GOPATH/bin $PATH
