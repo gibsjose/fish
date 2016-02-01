944f154b47a7d3c16f51757618a6e1b6c8a61a3a
Joe Gibson <gibsjose@mail.gvsu.edu>
2016-02-01 11:03:09 -0500

    Changing git alias from 'git lg' to 'git ls'

--- aliases/aliases.fish
+++ aliases/aliases.fish
@@ -5,5 +5,5 @@ end
 
 # Setup Improved Git Log
 function setup-git-log
-    git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+    git config --global alias.ls "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
 end
