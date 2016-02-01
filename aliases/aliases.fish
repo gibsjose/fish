8ff4122cb6eb365008bb17ab6ce4e57fda07e535
Joe Gibson <gibsjose@mail.gvsu.edu>
2016-02-01 10:29:32 -0500

    Added setup for better git log output

--- aliases/aliases.fish
+++ aliases/aliases.fish
@@ -2,3 +2,8 @@
 function microamps
     ssh -o ServerAliveInterval=20 -c blowfish -i ~/.ssh/microamps microamps@microamps.ydns.eu $argv
 end
+
+# Setup Improved Git Log
+function setup-git-log
+    git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
+end
