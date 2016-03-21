e7c7521cfdcbfb72e3664483b388b251e77bb7cf
Joe Gibson <joseph.gibson@nasa.gov>
2016-03-21 10:09:07 -0400

    Adding function for transfer.sh

--- functions/utilities.fish
+++ functions/utilities.fish
@@ -31,3 +31,27 @@ end
 function fuck
     eval (thefuck --alias | tr '\n' ';')
 end
+
+# Transfer
+function transfer
+    if not count $argv >/dev/null
+        echo "No arguments specified."
+        echo ""
+        echo "Usage:"
+        echo -e "  1)   transfer /tmp/test.md"
+        echo -e "  2)   cat /tmp/test.md | transfer test.md"
+        return 1
+    end
+
+    set tmpfile (mktemp -t transferXXX)
+
+    if tty -s
+        set basefile (basename "$argv[1]" | sed -e 's/[^a-zA-Z0-9._-]/-/g')
+        curl --progress-bar --upload-file "$argv[1]" "https://transfer.sh/$basefile" >> $tmpfile
+    else
+        curl --progress-bar --upload-file "-" "https://transfer.sh/$argv[1]" >> $tmpfile
+    end
+
+    cat $tmpfile
+    rm -f $tmpfile
+end
