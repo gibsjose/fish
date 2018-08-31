2d572c011ca54528cfb45b1fa94ac4de28bc26ff
Joe Gibson <joseph.gibson@nasa.gov>
2018-08-31 12:09:43 -0400

    Configure paths

--- config.fish
+++ config.fish
@@ -10,12 +10,21 @@ set -g -x PATH ~/bin/util.git-hfcs $PATH
 ### Todo Python script
 # set -g -x PATH ~/bin/todo $PATH
 
+### Rust
+set -g -x PATH ~/.cargo/bin $PATH
+
+### bin2image
+set -g -x PATH ~/bin/util.bin2image $PATH
+
+### Rocket
+set -g -x PATH ~/bin/util.rocket/bin $PATH
+
 ### CodeSonar
 set -g -x PATH $PATH /usr/local/codesonar-4.2p0/codesonar/bin
 
 ### AVR Toolchain
 #### Postfixing this to PATH so it searches this last
-set -g -x PATH $PATH /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin
+# set -g -x PATH $PATH /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin
 
 ### MacTeX (pdflatex, etc.)
 set -g -x PATH /Library/TeX/texbin $PATH
