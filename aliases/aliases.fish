e2bb4230e60f1b7167f7875139b59a185153b3a9
Joe Gibson <joseph.gibson@proton.me>
2023-04-02 01:51:59 -0400

    Clean up fish config/aliases

--- aliases/aliases.fish
+++ aliases/aliases.fish
@@ -1,6 +1,10 @@
 # `cat` to `bat`
 function cat
-    command bat $argv
+    if type -q bat
+        command bat --style=plain $argv
+    else
+        command cat $argv
+    end
 end
 
 # `curl` to `httpie`
@@ -29,40 +33,33 @@ end
 
 # `ls` to `exa`
 function ls
-    command exa $argv
+    if type -q exa
+        command exa $argv
+    else
+        command ls $argv
+    end
 end
 
 # ls with git output
 function lsg
-    command exa --long --git $argv
-end
-
-# `ls` helper functions
-function la
-    ls -la $argv
+    if type -q exa
+        command exa --long --git $argv
+    else
+        command ls $argv
+    end
 end
 
 function l
     ls $argv
 end
 
-function pls
-    pwd; echo; ls $argv
-end
-
 # Push/Pop Directories
 function pud
     pushd $argv
 end
-function d
-    pud $argv
-end
 
 function pod
     popd $argv
-end
-function p
-    pod $argv
 end
 
 # Tree
@@ -71,7 +68,6 @@ function tree
 end
 
 function t --argument-names 'level'
-
     if test -n "$level"
         tree -L "$level"
     else
@@ -81,58 +77,4 @@ end
 
 function pt
     pwd; t
-end
-
-# `emacs` wrapper
-function emacsclient
-    ec $argv
-end
-
-# `s` aliases
-function search
-    s $argv
-end
-
-function stackoverflow
-    s -p stackoverflow $argv
-end
-
-function stack
-    s -p stackoverflow $argv
-end
-
-function google
-    s -p google $argv
-end
-
-function wikipedia
-    s -p wikipedia $argv
-end
-
-function wiki
-    s -p wikipedia $argv
-end
-
-function amazon
-    s -p amazon $argv
-end
-
-function github
-    s -p github $argv
-end
-
-function hackernews
-    s -p hackernews $argv
-end
-
-function hn
-    s -p hackernews $argv
-end
-
-function wolframalpha
-    s -p wolframalpha $argv
-end
-
-function wolfram
-    s -p wolframalpha $argv
 end
--- config.fish
+++ config.fish
@@ -1,41 +1,9 @@
 # PATH
 set -g -x PATH /usr/local/bin $PATH
-
-### User Binaries
 set -g -x PATH ~/bin $PATH
-### Utilities
 set -g -x PATH ~/bin/utilities $PATH
-### Git Merge/Diff Tools
-# set -g -x PATH ~/bin/git-tools $PATH
-### Todo Python script
-# set -g -x PATH ~/bin/todo $PATH
-
-### Rust
 set -g -x PATH ~/.cargo/bin $PATH
-
-### AVR Toolchain
-# set -g -x PATH /Applications/Arduino.app/Contents/Java/hardware/tools/avr/bin $PATH
-
-### General Coding Tools (clang, etc.)
-# set -g -x PATH ~/bin/clang-analyzer $PATH
-### Pipsi
 set -g -x PATH ~/.local/bin $PATH
-### ROOT
-# set -g -x PATH /usr/local/root/bin $PATH
-### Add RVM to PATH for scripting
-# set -g -x PATH $HOME/.rvm/bin $PATH
-### Go
-set -g -x GOPATH $HOME/go
-set -g -x PATH $GOPATH $GOPATH/bin $PATH
-set -g -x PATH /usr/local/opt/go/libexec/bin $PATH
-# @TODO Cannot source a bash file from fish...
-# source $GOPATH/src/github.com/zquestz/s/autocomplete/s-completion.bash
-
-### MacTeX (pdflatex, etc.)
-set -g -x PATH /Library/TeX/texbin $PATH
-
-# Brew Cask Options
-set -g -x HOMEBREW_CASK_OPTS '--appdir=/Applications'
 
 # Set Editor
 set -g -x EDITOR 'vim'
@@ -45,9 +13,6 @@ set -g -x fish_greeting ''
 
 # Source the autojump setup
 [ -f /usr/local/share/autojump/autojump.fish ]; and source /usr/local/share/autojump/autojump.fish
-
-# Source `pass` completion
-source /usr/local/share/fish/vendor_completions.d/pass.fish
 
 # Source aliases
 for f in (find ~/.config/fish/aliases/*.fish)
@@ -59,15 +24,7 @@ for f in (find ~/.config/fish/functions/*.fish)
     source $f
 end
 
-# Source API Tokens
-# for f in (find ~/.config/fish/tokens/*.fish)
-#     source $f
-# end
-
-# Set path for Git HFCS
-set -g -x PATH /usr/local/bin $PATH
-
 # Redefine fish prompt to use powerline
 function fish_prompt
-    powerline-shell --shell bare $status
+    ~/.local/bin/powerline-shell --shell bare $status
 end
