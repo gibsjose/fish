bc2eb727dac36730e62f9bb112ab704ed29f1600
Joe Gibson <joseph.gibson@cern.ch>
2025-10-03 12:15:20 +0200

    Remove deprecated SSH aliases (prefer actual SSH config file)

--- aliases/ssh.fish
+++
@@ -1,8 +0,0 @@
-#######################################
-# SSH Aliases
-#######################################
-
-# Microamps Blog
-function microamps
-    ssh -o ServerAliveInterval=20 -i ~/.ssh/microamps microamps@microamps.ydns.eu $argv
-end
