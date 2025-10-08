ca9505e65c96ec227af84a6c7f7975598d7905fc
Joe Gibson <joseph.gibson@cern.ch>
2025-10-08 15:24:59 +0200

    Add OTP function to grab OTP from 1password-cli and save to clipboard

--- functions/utilities.fish
+++ functions/utilities.fish
@@ -6,7 +6,7 @@ end
 # Setup Improved Git Environment
 function setup-git
     git config --global alias.ls "log --color --graph --pretty=format:'%Cred%h%Creset -%C(blue)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
-    
+
     git config --global alias.sl "stash list --pretty=format:'%C(bold blue)%gd%Cred: %C(reset)%s'"
 end
 
@@ -30,4 +30,34 @@ end
 
 function dirsize
     du -sh $argv | awk '{print $1}'
+end
+
+# Extract OTP from 1Password and copy to clipboard
+function otp
+    if test (count $argv) -eq 0
+        echo "Usage: otp <item_name>"
+        echo "Example: otp 'My Bank Account'"
+        return 1
+    end
+
+    # Get the OTP from 1Password
+    set otp_code (op item get "$argv[1]" --otp 2>/dev/null)
+
+    if test $status -ne 0
+        echo "Error: Failed to get OTP for '$argv[1]'"
+        echo "Make sure you're signed in to 1Password CLI and the item exists"
+        return 1
+    end
+
+    # Copy to clipboard
+    echo $otp_code | pbcopy
+
+    # Display the OTP (first 3 and last 3 characters for security)
+    set otp_length (string length $otp_code)
+    if test $otp_length -gt 6
+        set masked_otp (string sub -l 3 $otp_code)"***"(string sub -s (math $otp_length - 2) $otp_code)
+        echo "OTP copied to clipboard: $masked_otp"
+    else
+        echo "OTP copied to clipboard: $otp_code"
+    end
 end
