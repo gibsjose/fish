0227ece7788268fc3a90b2d0b8205dcd373e038e
Joe Gibson <joseph.gibson@cern.ch>
2025-10-31 16:05:05 +0100

    Modify config for Thorium

--- config.fish
+++ config.fish
@@ -1,9 +1,18 @@
+# Launch tmux if in an interactive shell
+# if status is-interactive
+# and not set -q TMUX
+#     exec tmux new-session -A -s main
+# end
+
 # PATH
 set -g -x PATH /usr/local/bin $PATH
 set -g -x PATH ~/bin $PATH
 set -g -x PATH ~/bin/utilities $PATH
 set -g -x PATH ~/.cargo/bin $PATH
 set -g -x PATH ~/.local/bin $PATH
+
+# JDK Path for compilers
+set -gx CPPFLAGS "-I/opt/homebrew/opt/openjdk@21/include"
 
 # Set Editor
 set -g -x EDITOR 'vim'
@@ -21,6 +30,11 @@ for f in (find ~/.config/fish/functions/*.fish)
     source $f
 end
 
+# Source any vendor completions
+for f in (find /opt/homebrew/share/fish/vendor_completions.d/*.fish)
+    source $f
+end
+
 # Redefine fish prompt to use powerline
 function fish_prompt
     ~/.local/bin/powerline-shell --shell bare $status
@@ -30,6 +44,13 @@ end
 if type -q fzf
     fzf --fish | source
 end
+
+# Open in tmux popup if on tmux, otherwise use --height mode
+# export FZF_DEFAULT_OPTS='--height 50% --tmux bottom,50% --layout reverse --border rounded'
+set -g -x FZF_DEFAULT_OPTS '--style full --layout reverse --tmux bottom,40%'
+
+# Print tree structure in the preview window
+set -g -x FZF_ALT_C_OPTS "--walker-skip .git,node_modules,target --preview 'tree -C {}'"
 
 # Set up 1Password CLI completions
 if type -q op
